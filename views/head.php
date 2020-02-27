09ffae10042cceb2134cf6e134ef7720974a8611
Wei-Hao Wang <wwang03@risd.edu>
2020-02-27 18:24:33 -0500

    1. connect open records generator; 2. make the scroller fetch name1 of children; 3. the content of scoller is shuffled, with a greeting and an ending message; 4. output_permitted_event is commented out for a moment. It's doing some weird thing and throw an error

--- views/head.php
+++ views/head.php
@@ -38,7 +38,7 @@ else
 ?><!DOCTYPE html>
 <html>
     <head>
-        <title><? echo $site_name; ?></title>
+        <title><? echo $title; ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="apple-mobile-web-app-capable" content="yes">
--- views/home.php
+++ views/home.php
@@ -10,14 +10,15 @@ array_push($msg_array, ' currently ' . $output['wind_string']);
 array_push($msg_array, ' /// Currently ' . $output['temp_f'] . ' degrees.');
 array_push($msg_array, ' There are trains arriving at: ' . $output_train.".");
 array_push($msg_array, ' ' . $output_jobs["job_agency"] . ' is hiring ' . $output_jobs["job_title"] . " at " . $output_jobs["job_division"] . ", " . $output_jobs["job_location"] . ".");
-array_push($msg_array, ' ' . $output_permitted_event["event_name"] . ' will be happening from ' . $output_permitted_event["event_start_time"] . " until " . $output_permitted_event["event_end_time"] . ", at " . $output_permitted_event["event_location"] . ".");
+// array_push($msg_array, ' ' . $output_permitted_event["event_name"] . ' will be happening from ' . $output_permitted_event["event_start_time"] . " until " . $output_permitted_event["event_end_time"] . ", at " . $output_permitted_event["event_location"] . ".");
 
 shuffle($msg_array);
 
-foreach($msg_array as $ma)
+foreach($msg_array as $ma){
 	$msg .= $ma;
+}
 
-$msg = $msg_beginning . $msg . $msg_ending;
+// $msg = $msg_beginning . $msg . $msg_ending;
 
 ?>
 
