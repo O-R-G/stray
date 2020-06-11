c6843e1b970769dcf361a80761eafb37842efdf8
Wei-Hao Wang <wwang03@risd.edu>
2020-06-11 16:33:05 -0400

    style

--- views/home.php
+++ views/home.php
@@ -1,6 +1,5 @@
 <div id="mask">
-    <div id="display">
-    </div>   
+    <div id="display"></div>   
 </div>
 <script src='/static/js/msgs.js'></script>
 <!-- <script src='/static/js/matrix.js'></script> -->
