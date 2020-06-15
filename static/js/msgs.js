f3122239c0e1b2e72a0e1a5e54b10de930f9b03c
Wei <weiwangahsbeenused@gmail.com>
2020-06-15 13:31:58 +0000

    cleanup index and static/js/msgs

--- static/js/msgs.js
+++ static/js/msgs.js
@@ -1,11 +1,11 @@
-/* 
+/*
     collect and assemble msg[] for matrix.js
     includes cacheing
-*/    
+*/
 
 // date / time
 
-Date.prototype.today = function () { 
+Date.prototype.today = function () {
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     return days[this.getDay()] + " " + this.getDate() + " " + months[this.getMonth()] + " " + this.getFullYear();
@@ -33,7 +33,7 @@ var now_hr = now.hour();
 var now_min = now.minute();
 
 var req_array = [
-	{	
+	{
 		'name': 'wet-letters',
 		'req_url': 'http://wetwords.o-r-g.net/now'
 		// 'req_url': 'static/data/dummy.json'
@@ -44,75 +44,10 @@ var now_msg = get_time();
 var msgs, // the final msgs for display. array of letters
 	msgs_sections = {}, // the kept msgs in the form of opening, mid, ending. it needs to stay array so that it has the flexibility to be updated.
 	msgs_temp = []; // the intermediate msgs to hold updated msgs, and wait until the current frame is settled. 
-var msgs_array = [], 
+var msgs_array = [],
 	msgs_array_temp = [];
-
 var sDisplay = document.getElementById('display');
 var interval = 1000;
-// msgs_opening_1.push('––––––––––––––––––––'); // en-dash (S)
-// msgs_opening_1.push('————————————————————'); // em-dash (M)
-/*
-msgs_opening_1.push('•••••••••••••••••••••');    // bullet (L)
-msgs_opening_1.push('•••••••••••••••••••••');    // bullet (L)
-msgs_opening_1.push('•••••••••••••••••••••');    // bullet (L)
-msgs_opening_1.push('•••••••••••••••••••••');    // bullet (L)
-*/
-
-// msgs_sections['opening'] = [];
-// msgs_sections['opening'][0] = [];
-// msgs_sections['opening'][0].push('NEW YORK CONSOLIDATED'); 
-// msgs_sections['opening'][0].push('                     '); 
-// msgs_sections['opening'][0].push('                     '); 
-// msgs_sections['opening'][0].push('                     '); 
-// msgs_sections['opening'][0] = msgs_sections['opening'][0].join('');
-// msgs_sections['opening'][1] = [];
-// msgs_sections['opening'][1].push(now_msg[0]); 
-// msgs_sections['opening'][1].push(now_msg[1]); 
-// msgs_sections['opening'][1].push('–––––––––––––––––––––'); 
-// msgs_sections['opening'][1].push('—————————————————————'); 
-// msgs_sections['opening'][1] = msgs_sections['opening'][1].join('');
-/*
-msgs_opening_3.push('Nueva York           '); 
-msgs_opening_3.push('Consolidada          '); 
-msgs_opening_3.push('紐約合作基金會              '); 
-msgs_opening_3.push('                     '); 
-msgs_opening_3.push('      نيويورك الموحدة');
-msgs_opening_3.push('                     '); 
-msgs_opening_3.push('...........hello?....'); 
-msgs_opening_3.push('....:)...............');
-msgs_opening_3 = msgs_opening_3.join('');
-msgs_opening = msgs_opening_1.concat(msgs_opening_2, msgs_opening_3);
-*/
-
-/*
-// add these to o-r-g amd retrieve?
-// or store in cache txt files?
-// in msgs.js?
-$msgs_temp = [];
-$msgs_temp[] = 'UNIQUE COPY CENTER   ';
-$msgs_temp[] = '     No Thin Special ';
-$msgs_temp[] = 'STORE for RENT call  ';
-$msgs_temp[] = '(347) 680-3340 / / . ';
-$msgs_temp[] = 'Tai Loong Landromat  ';
-$msgs_temp[] = '> Work-in-Progress 合 ';
-*/
-
-// msgs_sections['mid'] = {};
-
-// msgs_sections['ending'] = ' 0 1 2 3 4 5 6 7 8 9 Have a nice day.';
-// var msgs_break = '///';
-
-// var timer;
-// var msg, msg_temp;
-// var letters = [];
-// var words = [];
-
-// preventing from the animation starts before data loaded.
-// if ready_now == 0 when an api is loaded, 
-// it fires timer then reading_now++
-// ( json.js )
-// var ready_now = 0;
-// var ready_full = req_array.length;
 
 function handle_msgs(name, response, results_count = false){
 	// console.log('handle_msgs...'+name);
@@ -125,207 +60,20 @@ function handle_msgs(name, response, results_count = false){
 		var poem_arr = poem.split('');
 		sDisplay.innerHTML = poem_arr[current_position];
 		current_position++;
+
+		// wait until exactly next x seconds, advance current letter+=x
+		// see https://stackoverflow.com/questions/10795164/accurately-run-a-function-when-the-minute-changes
+
 		setInterval(function(){
 			sDisplay.innerHTML = poem_arr[current_position];
 			current_position++;
 			if(current_position >= poem_arr.length)
 				current_position = 0;
 		}, interval);
-
 	}
-
-	// opening msg for each section;
-	// if(name == 'new-york-times'){
-	// 	// console.log('updaing new-york-times');
-	// 	this_msgs = [' from the NYTimes : ' + msgs_break ];
-	// 	response = response['results'] ;
-	// 	for(i = 0 ; i < results_count ; i++){
-	// 		var this_msg = response[i]['title'];
-	// 		if(typeof this_msg != 'undefined')
-	// 			this_msgs.push(this_msg+msgs_break);
-	// 	}
-
-	// }else if(name == 'covidtracking'){
-	// 	// console.log('updaing covidtracking');
-	// 	this_msgs.push(' from covidtracking.com : ' + msgs_break);
-	// 	for(i = 0 ; i < response.length ; i++){
-	// 		if(response[i]['state'] == 'NY'){
-	// 			response = response[i];
-	// 			break;
-	// 		}
-	// 	}
-	// 	if(typeof response['positive'] != 'undefined')
-	// 		this_msgs.push('Positive cases: '+response['positive'] + msgs_break+' ');
-	// 	if(typeof response['negative'] != 'undefined')
-	// 		this_msgs.push('Negative cases: '+response['negative'] + msgs_break+' ');
-	// 	if(typeof response['hospitalizedCurrently'] != 'undefined')
-	// 		this_msgs.push('Currently hospitalized cases: '+response['hospitalizedCurrently'] + msgs_break+' ');
-
-	// }else if(name == '311'){
-	// 	for(i = 0 ; i < response.length ; i++){
- //        	var this_msg = ' from '+response[i]['agency']+': ';
- //        	this_msg += response[i]['descriptor']+' is reported around '+response[i]['landmark']+' ';
- //        	this_msgs.push( msgs_break+this_msg.toUpperCase()+msgs_break );
- //        }
-
-	// }else if(name == 'temp'){
-	// 	var oParser = new DOMParser();
-	// 	var oDOM = oParser.parseFromString(response, "application/xml");
-	// 	var current = oDOM.getElementsByTagName('weather')[0];
-	// 	var temp_f = oDOM.getElementsByTagName('temp_f')[0];
-	// 	var temp_c = oDOM.getElementsByTagName('temp_c')[0];
-	// 	var wind_string = oDOM.getElementsByTagName('wind_string')[0];
-	// 	if(typeof temp_f != 'undefined')
-	// 		this_msgs.push( ' Currently ' + temp_f.innerHTML + '°....' + msgs_break );
-	// 	if(typeof wind_string != 'undefined')
-	// 		this_msgs.push( ' Winds ' + wind_string.innerHTML + msgs_break );
-
-	// }
-	// else if(name == 'train'){
-	// 	this_msgs = [' There is a train arriving now at : ' + response['result'][0]['name'] + ". " + msgs_break ];
-	// }
-	// else if(name == 'population'){
-	// 	// console.log(response[0]);
-	// 	this_msgs = [' Total population in NYC: ' + response[0]['_2020']+". " + msgs_break ];
-	// 	for(i = 1 ; i <response.length ; i++ ){
-	// 		this_msgs.push(' Population in ' + response[i]['borough'].replace('   ', '') + ": " + response[i]['_2020']+"("+response[i]['_2020_boro_share_of_nyc_total']+"%)"+msgs_break );
-	// 	}
-	// }
-	// else if(name == 'hotspot'){
-	// 	var index = parseInt( response.length * Math.random() );
-	// 	var data_count = 0;
-	// 	this_msgs = [];
-	// 	while(data_count < 1){			
-	// 		if(response[index]['type'] == 'Free'){
-	// 			this_msgs.push(' Free public hotspot "'+response[index]['ssid']+'" at '+response[index]['location']);
-	// 			data_count++;
-	// 		}
-	// 		index = parseInt( response.length * Math.random() );
-	// 	}
-	// }
-	// else if(name == 'restaurant-inspection'){
-	// 	var index = parseInt( response.length * Math.random() );
-	// 	var data_count = 0;
-	// 	results_count = results_count ? results_count : 1;
-	// 	this_msgs = [];
-	// 	this_msgs = [' From DOHMH New York City Restaurant Inspection Results : ' ];
-		
-	// 	while(data_count < results_count){
-	// 		if(response[index]['critical_flag'] == 'N' && response[index]['grade'] == 'A'){
-	// 			this_msgs.push(response[index]['dba'] + ' on '+ response[index]['street']+' is graded as A. '+msgs_break);
-	// 			data_count++;
-	// 		}
-	// 		index = parseInt( response.length * Math.random() );			
-	// 	}
-	// }
-	// else if(name == 'street-tree'){
-	// 	var index = parseInt( response.length * Math.random() );
-	// 	var data_count = 0;
-	// 	results_count = results_count ? results_count : 1;
-	// 	this_msgs = [];
-	// 	this_msgs = [' Street Tree in NYC : '];
-		
-	// 	while(data_count < results_count){
-	// 		this_msgs.push(response[index]['spc_common'] + ' on '+ response[index]['address']+'. Diameter at Breast Height: '+response[index]['tree_dbh']+' in. '+msgs_break);
-	// 		data_count++;
-	// 		index = parseInt( response.length * Math.random() );			
-	// 	}
-	// }
-	// else if(name == 'air-quality'){
-	// 	var aqi = response['data']['aqi'];
-	// 	var level = '';
-	// 	if(aqi <= 50){
-	// 		level = 'GOOD';
-	// 	}else if(aqi <= 100){
-	// 		level = 'Moderate';
-	// 	}else if(aqi <= 150){
-	// 		level = 'Unhealthy for Sensitive Groups';
-	// 	}else if(aqi <= 200){
-	// 		level = 'Unhealthy';
-	// 	}else if(aqi <= 300){
-	// 		level = 'Very Unhealthy';
-	// 	}else{
-	// 		level = 'Hazardous';
-	// 	}
-	// 	this_msgs = [];
-	// 	this_msgs = [' Air Quality Index in New York : ' ];
-		
-	// 	this_msgs.push(aqi + ' ('+ level+') '+msgs_break);
-	// }
-	// else if(name == 'energy-efficiency-projects'){
-	// 	var index = parseInt( response.length * Math.random() );
-	// 	var data_count = 0;
-	// 	results_count = results_count ? results_count : 1;
-	// 	this_msgs = [];
-	// 	this_msgs = [" The City's Energy Efficiency Projects: " ];
-	// 	while(data_count < results_count){
-	// 		if(response[index]['projectsitename'] && response[index]['primaryprojecttype']!='Other'){
-	// 			this_msgs.push(response[index]['primaryprojecttype'] + ' for '+ response[index]['projectsitename']+'. Status: '+response[index]['projectstatus']+' '+msgs_break);
-	// 			data_count++;
-	// 		}
-	// 		index = parseInt( response.length * Math.random() );			
-	// 	}
-	// }
-	// else if(name == 'DCA-license'){
-	// 	var index = parseInt( response.length * Math.random() );
-	// 	var data_count = 0;
-	// 	results_count = results_count ? results_count : 1;
-	// 	this_msgs = [];
-	// 	this_msgs = [" From DCA : Legally Operating Businesses License issued to" ];
-		
-	// 	while(data_count < results_count){
-	// 		if(response[index]['status']=='Issued'){
-	// 			this_msgs.push(response[index]['business_name']+' '+msgs_break);
-	// 			data_count++;
-	// 		}
-	// 		index = parseInt( response.length * Math.random() );			
-	// 	}
-	// }
-	// msgs_sections['mid'][name] = this_msgs;
-	
-	// update_msgs();
 }
 
 function next_letter(arr){
-	
+	// do nothing
 }
 
-// function shuffle(array) {
-//     array.sort(() => Math.random() - 0.5);
-// }
-
-// function update_msgs(isBeginning = false){
-// 	// console.log('update_msgs...');
-// 	msgs_mid_array = Object.keys(msgs_sections['mid']).map(function (key) { 
-//         return msgs_sections['mid'][key]; 
-//     }); 
-// 	if(isBeginning)
-// 		shuffle(msgs_mid_array);
-
-// 	msgs_temp = [msgs_sections['opening']];
-// 	for(i = 0 ; i < msgs_mid_array.length ; i++){
-// 		for(j = 0 ; j < msgs_mid_array[i].length ; j++)
-// 			msgs_temp.push(msgs_mid_array[i][j]);
-// 	}
-// 	msgs_temp.push(msgs_sections['ending']);
-
-// 	msgs_array_temp = msgs_temp;
-// 	msgs_temp = msgs_temp.join('');
-// 	msg_temp = msgs_temp.substr(pointer,columns*rows).split('');
-// 	msgs_temp = msgs_temp.toUpperCase();
-// 	msgs_temp = msgs_temp.split('');
-// }
-
-// this is different from update_msgs() (at least for now)
-// update_msgs(): fired every whatever seconds setInverval sets;
-// update_msgs_opening(): fired every time the msgs loop is done;
-// function update_msgs_opening(){
-// 	now_msg = get_time();
-// 	msgs_sections['opening'][1] = [];
-// 	msgs_sections['opening'][1].push(now_msg[0]); 
-// 	msgs_sections['opening'][1].push(now_msg[1]); 
-//     msgs_sections['opening'][1].push('–––––––––––––––––––––'); // en-dash (S)
-//     msgs_sections['opening'][1].push('—————————————————————'); // em-dash (L)
-// 	msgs_sections['opening'][1] = msgs_sections['opening'][1].join('');
-// 	msgs_temp[0] = msgs_sections['opening'][0].concat(msgs_sections['opening'][1]);
-// }
