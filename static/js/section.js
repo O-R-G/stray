72db1d4c3a11e841b61b3c0767891d245d359de4
Wei-Hao Wang <wwang03@risd.edu>
2020-10-27 15:01:43 +0800

    add untracked files

---
+++ static/js/section.js
@@ -0,0 +1,35 @@
+// key = chapter
+// image = array of folios of images, including blanks between images
+
+var sections_info = {
+	1: {
+		'image': ['119-134', '156-158', 359, 490, '494-509', '566-578', '581-587']
+	},
+	2: {
+		'image': ['26-28']
+	}
+	,
+	3: {
+		'image': [0, '289-308']
+	}
+	,
+	4: {
+		'image': []
+	}
+	,
+	5: {
+		'image': []
+	}
+	,
+	6: {
+		'image': [142, '242-243', '382-403']
+	}
+	,
+	7: {
+		'image': []
+	}
+	,
+	8: {
+		'image': ['21-99']
+	}
+};
