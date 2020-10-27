d76592e145e2a0cfe76cbecee03b492f680be614
Wei-Hao Wang <wwang03@risd.edu>
2020-10-27 16:14:26 +0800

    use js to get query string

--- views/slide-text.php
+++ views/slide-text.php
@@ -7,10 +7,21 @@
 <div id = 'control_display'></div>
 <div id="display"><img id = 'display_img' src = ''></div>
 <script>
+	function getParameterByName(name, url = window.location.href) {
+	    name = name.replace(/[\[\]]/g, '\\$&');
+	    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+	        results = regex.exec(url);
+	    if (!results) return null;
+	    if (!results[2]) return '';
+	    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+	}
 	var template = 'slide-text';
-	var chapter = <?= $chapter; ?>;
-	var section = '<?= $section; ?>';
-	var type = '<?= $type; ?>';
+	// var chapter = <?= $chapter; ?>;
+	// var section = '<?= $section; ?>';
+	// var type = '<?= $type; ?>';
+	var chapter = getParameterByName('chapter');
+	var section = getParameterByName('section');
+	var type = getParameterByName('type');
     // console.log(chapter);
 </script>
 <script src='/static/js/section.js'></script>
