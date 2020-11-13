237394ae22052fa041ecbb5fce13033d85092f33
Wei-Hao Wang <wwang03@risd.edu>
2020-11-13 16:27:37 +0800

    turn off o-r-g

--- views/head.php
+++ views/head.php
@@ -1,39 +1,39 @@
 <?
 // path to config file
-$config = $_SERVER['DOCUMENT_ROOT']."/open-records-generator/config/config.php";
-require_once($config);
+// $config = $_SERVER['DOCUMENT_ROOT']."/open-records-generator/config/config.php";
+// require_once($config);
 
-// specific to this 'app'
-$config_dir = $root."/config/";
-require_once($config_dir."url.php");
-require_once($config_dir."request.php");
+// // specific to this 'app'
+// $config_dir = $root."/config/";
+// require_once($config_dir."url.php");
+// require_once($config_dir."request.php");
 
-require_once("lib/lib.php");
+// require_once("lib/lib.php");
 
-$db = db_connect("guest");
+// $db = db_connect("guest");
 
-$oo = new Objects();
-$mm = new Media();
-$ww = new Wires();
-$uu = new URL();
-$rr = new Request();
+// $oo = new Objects();
+// $mm = new Media();
+// $ww = new Wires();
+// $uu = new URL();
+// $rr = new Request();
 
-// self
-if($uu->id)
-    $item = $oo->get($uu->id);
-else
-    $item = $oo->get(0);
-$name = ltrim(strip_tags($item["name1"]), ".");
+// // self
+// if($uu->id)
+//     $item = $oo->get($uu->id);
+// else
+//     $item = $oo->get(0);
+// $name = ltrim(strip_tags($item["name1"]), ".");
 
-// document title
-$item = $oo->get($uu->id);
-$title = $item["name1"];
+// // document title
+// $item = $oo->get($uu->id);
+// $title = $item["name1"];
 $site_name = "Stray";
 
-if ($title)
-    $title = $site_name." | ".strip_tags($title);
-else
-    $title = $site_name;
+// if ($title)
+//     $title = $site_name." | ".strip_tags($title);
+// else
+//     $title = $site_name;
 
 // query strings        ** dev **
 $current_directory = dirname($_SERVER['SCRIPT_NAME']);
