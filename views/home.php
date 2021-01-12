4f4a49f03f7e0808b143e726f4ea2d11517a0181
Wei-Hao Wang <wwang03@risd.edu>
2021-01-12 15:34:29 +0800

    first commit

--- views/home.php
+++ views/home.php
@@ -1,7 +1,187 @@
-<div id="home_container">
-      <a id = 'txt-link' class = 'main-link' href='javascript:open_duo();'><img src = 'media/png/macos-document.png'></a><a id = 'image-link' class = 'main-link' href='javascript:open_duo();'><img src = 'media/png/macos-photo.png'></a>
-      <a id = 'colophon-link' href='javascript:popup("colophon", "", "");'>Colophon</a><br>
-      <a id = 'radio-link' href='javascript:popup("radio", "", "");'>Radio</a>
-</div>   
+<? $radio_words_raw = array(
+		'STRAY ERR | STRAY (STRĀ) INTR.V. STRAYED, STRAY•ING, STRAYS 1A. TO MOVE AWAY FROM A GROUP, DEVIATE FROM THE CORRECT COURSE, OR GO BEYOND ESTABLISHED LIMITS. B. TO BECOME LOST. 2. TO WANDER ABOUT WITHOUT A DESTINATION OR PURPOSE; ROAM. SEE SYNONYMS AT WANDER. 3. TO FOLLOW A WINDING COURSE; MEANDER. 4. TO DEVIATE FROM A MORAL, PROPER, OR RIGHT COURSE; ERR. 5. TO BECOME DIVERTED FROM A SUBJECT OR TRAIN OF THOUGHT; DIGRESS: STRAYED FROM THE TOPIC. SEE SYNONYMS AT SWERVE. ◊ N. ONE THAT HAS STRAYED, ESPECIALLY A DOMESTIC ANIMAL WANDERING ABOUT. ◊ ADJ. 1. STRAYING OR HAVING STRAYED; WANDERING OR LOST: STRAY CATS AND DOGS. 2. SCATTERED OR SEPARATE: A FEW STRAY CRUMBS. [MIDDLE ENGLISH STRAIEN, FROM OLD FRENCH ESTRAIER, FROM ESTREE, HIGHWAY, FROM LATIN STRĀTA. SEE STREET.] — STRAY’ER | ENTER THE PHOTORAMA ΦΩΤΟΣ (PHŌTOS) GENITIVE OF ΦΩ͂ (PHŌS) LIGHT ΓΡΑΦΗ (GRAPHÉ) REPRESENTATION BY MEANS OF LINES" OR "DRAWING", TOGETHER MEANING DRAWING WITH LIGHT. MODIFICATION OF - ORAMA FROM ANCIENT GREEK ὍΡᾹΜᾸ, (HÓRĀMA, “SIGHT, SPECTACLE”) TEMPLE HI LO | STRAY | INDEFINITE TIME INDETERMINATE TIME DISPLACED TIME ABSTRACT TIME BILATERAL TIME TRANSMISSION TIME TRANSDUCED TIME | SINGULARITIES | ARTICULATION OF SOUND FORMS IN TIME | WHEN YOU WRITE A POEM YOU USE SOUNDS AND WORDS OUTSIDE TIME. YOU USE TIMELESS ARTICULATIONS. | SPLAY ANTHEM | SONG OF THE ANDOUMBOULOU 50 | ERODING WITNESS | SOUND AND SENTIMENT, SOUND AND SYMBOL | POETIC LANGUAGE IS LANGUAGE OWNING UP TO BEING AN ORPHAN, TO ITS TENUOUS KINSHIP WITH THE THINGS IT OSTENSIBLY REFERS TO. | THE POLITICS OF POETIC FORM: POETRY AND PUBLIC POLICY | FRIENDS IN DEED | FRIENDS IN DEED | FRIENDS IN DEED | GRACE WALKING AND TALKING | BOMBARDO | MY EMILY DICKINSON | MAGNALIA CHRISTI AMERICANA | BREATHING, BOMBS, SWORDS, DEATH, SPEARS AND FLAMES | WILL AND BE GOING TO | WILL | BE GOING TO | SIGNAL ESCAPES | SINGULARITIES | TREE ELLIPSES OR THE HOT WAR COUPLE',
+		'THE SEVEN SLEEPERS | SEPTET FOR THE END OF TIME | SEPTET FOR THE END OF TIME | SEPTET FOR THE END OF TIME | SPLAY ANTHEM | SPLAY ANTHEM | BYG | ',
+		'A SIDE / B SIDE | EARLY SHAKER SPIRITUALS, A RECORD ALBUM INTERPRETATION | THE B SIDE: NEGRO FOLKLORE FROM TEXAS STATE PRISONS | THE B SIDE | TWO PLAYS ABOUT AMERICAN FOLKLORE DRAWN OUT OVER TIME AND SPACE ONE BLACK AND ONE WHITE DISCREPANTLY ENGAGING AS NATHANIEL MACKEY WOULD CALL IT BOTH RECORD ALBUM INTERPRETATIONS BOTH REAL AND IMAGINED WALK THROUGH ONE DOOR INTO A CIRCLE IN A SQUARE INTO THE EARLY SHAKER SPIRITUALS A RITUAL COMMUNITY OF CAST OUTS FROM THE CHURCH OF ENGLAND FOR PRACTICING THEIR RADICAL BELIEFS SCORNED AND THREATENED THEY RESETTLED IN THE NEW AMERICAN WILDERNESS OF THE 18TH CENTURY SEEKING RELIGIOUS FREEDOM IN ECSTATIC SONG AND DANCE TO UNSETTLE AND UNRESTRELIGEOUS FORM. WALK THROUGH ANOTHER DOOR INTO ANOTHER CIRCLE INSIDE ANOTHER SQUARE IN THE BLACK BOX THEATER AND ENTER THE B SIDE: NEGRO FOLKLORE FROM TEXAS STATE PRISONS. ENTER A YOUNG ACTOR ALONE IN HIS BEDROOM TRANSCRIBING, RESEARCHING AND INTERPRETING WORK-SONGS, SPIRITUALS, BLUES AND TOASTS. CONVICT LYRICS AN AMERICAN ORAL TRADITION OF HARD LABOR ROOTED IN THE EARLY DAYS OF SLAVERY. | THE SENSATIONS | THE TRANSDUCTIONS | TRANSDUCTION AS IT RELATES TO THE PHYSIOLOGICAL FOLLOWS THESE PRINCIPAL STEPS OF SENSORY PROCESSING: SIGNAL (SUN OR LP) + COLLECTION (EYE OR CAMERA OR EAR) + TRANSDUCTION (NERVOUS SYSTEM) + PROCESSING (LISTENING) + ACTION (SONG).',
+		'SPLAY ANTHEM | AMERICAN PHOTO - GRAPHS | AIDS | SOIL EROS - ION | SOIL EROSION | FSA | FSA | AMERICAN PHOTOGRAPHS | ION | ACTION | CONDITION | COMMUNION | UNION | SOIL EROSION | KNOCKING AROUND BETWEEN MONEY, SEX, AND BOREDOM: WALKER EVANS IN HAVANA AND NEW YORK | NOT LEAST SINCE THE YALE LECTURE ATTRACTED ITS SHARE OF OUTRAGE AND HATE MAIL. PERHAPS IT WAS TOO PROVOCATIVE FOR FORMER STUDENTS AND COLLEAGUES, RIGHT THERE WHERE HE HAD TAUGHT. YET, IT IS IN FACT THE HAGIOGRAPHIC VIEW OF EVANS THAT DIMINISHES HIM, RATHER THAN THE ATTEMPT TO PURSUE WHAT HAS BEEN EXCLUDED FROM THE APPROVED ENGAGEMENT WITH HIS PHOTOGRAPHS. HE IS SIMPLY NOT THE MONOLITHIC MORAL PARAGON OTHERS INVENTED––AND THAT, FOR ME, MAKES HIS WORK STRANGER AND MORE COMPELLING. | THE ORIGINAL CALL FOR SOIL EROSION SOUGHT TO MANEUVER AROUND ITS TITLE. IT SPOKE OF STATES EROSIONS, CLIMATE EROSIONS, CULTURAL EROSIONS AND SOCIAL EROSIONS. SOIL AS LAND, BODY AND SOILING. EROSION AS EROS, LOSS AND DESIRE AND ALSO THE POTENTIAL FOR NEW CLEARINGS. IN THE DAYS, WEEKS AND NOW YEARS FOLLOWING THE 2016 PRESIDENTIAL ELECTION, THIS PHRASEOLOGY HAS ENTERED SHIFTING LANDSCAPES THAT WITH EACH PASSING DAY GROW MORE AND MORE UNIMAGINABLE, WHERE ANY IDEA OF GROUND OR GROUNDING IS UNCLEAR AT BEST. TAKEN TOGETHER, THE WORK IN SOIL EROSION IS A COLLECTION OF THESE STARTING POINTS PLACED IN A LARGER NARRATIVE FIELD WHERE MEANING PLUS THE CULTURAL AND POLITICAL CIRCUMSTANCES OF OUR LIVING CONDITIONS GO TO WORK ON EACH OTHER TO CAST DOUBT AND SHORE UP NEW CERTAINTIES.',
+		'A GRAPHIC TONE | SPLAY ANTHEM, NEW DIRECTIONS',
+		'WET WORDS IN A HOT FIELD | WATER IN THE CAMERA IS A SIGN OF WHAT CAN HAPPEN WHEN WET WORDS ENTER A HOT FIELD. SUBJECTS WITHOUT CAMERAS IN A PLACE WITHOUT CAMERAS. SOME PEOPLE WILL SAY THAT THIS CAMERA IS A PRECISION INSTRUMENT BUT I SAY THAT THIS CAMERA IS AN INSTRUMENT FOR GOING STRAY. A CAMERA THAT IS STRAYING ON AN OBJECT OTHER THAN A HUMAN FACE MAY BE DETECTED AS A HUMAN FACE. | VIRTUAL HORIZONS IN EXPOSURE ZONES, NIGHT SCENES OR POINTS OF LIGHT, SUBJECTS IN LOW LIGHT, SUBJECTS AT THE EDGE OF THE PICTURE, SUBJECTS STRONGLY REFLECTING LIGHT, SUBJECTS MOVING DRAMATICALLY UP, DOWN, LEFT AND RIGHT, SUBJECTS THAT CHANGE SPEED AND MOVE ERRATICALLY, SUBJECTS APPROACHING OR MOVING AWAY FROM THE CAMERA, SUBJECTS AT THE EDGE OF THE PICTURE, SUBJECTS IN AN AUDIBLE FIELD, SUBJECTS GOING STRAY. | TOUCH THE SUBJECT TO FOCUS. GOING STRAY LIKE HAIRS ON THE NECK. PICTURE STRAY HAIRS CROSSING A SUBJECTS EYE AND THEN ERASING STRAY SUBJECTS FROM THE FRAME. HOW STRAY LIGHT REACHING THE IMAGE PLANE ENTERS THE CAMERA DURING EXPOSURE AND EVEN IF THE SUN IS SLIGHTLY AWAY FROM THE ANGLE OF VIEW IT MAY STILL CAUSE SMOKE OR FIRE. DRAGGING ON THE MONITOR WHILE LOOKING THROUGH THE VIEWFINDER STRAY SUBJECTS MOVING THROUGH THE FINDER MOVE WITH MANY SUBJECTS LOST & FOUND.'
+	);
+	$radio_words = '';
+	foreach($radio_words_raw as $chapter){
+		$chapter_temp = str_replace(' | ', ' ', $chapter);
+		$radio_words .= $chapter_temp . ' ';
+	}
 
+	$letter_image_filenames = scandir('media/letters');
+	$filenum_arr = array();
+	foreach($letter_image_filenames as $filename){
+		if(substr($filename, 0, 1) != '.'){
+			$this_key = explode('-', $filename);
+			$this_key = $this_key[0];
+			if(isset($filenum_arr[$this_key]))
+				$filenum_arr[$this_key] ++;
+			else
+				$filenum_arr[$this_key] = 1;
+		}
+	}
+?>
+<div id = 'radio_container'>
+	<img id = 'radio_image_0' class = 'radio_image not_current'>
+	<img id = 'radio_image_1' class = 'radio_image not_current'>
+	<img id = 'radio_image_2' class = 'radio_image not_current'>
+</div>
+<div id = 'nav'><a href = 'javascript:popup("colophon", "", "")'>COLOPHON</a></div>
+<script>
+	var current_letter;
+	var radio_words = '<?= $radio_words; ?>';
+	// console.log(radio_words.length);
+	var radio_image = document.getElementsByClassName('radio_image');
+	var radio_image_0 = document.getElementById('radio_image_0');
+	var radio_image_1 = document.getElementById('radio_image_1');
+	var radio_image_2 = document.getElementById('radio_image_2');
+	var image_counter = 0;
 
+	var filenum_arr = <? echo json_encode($filenum_arr); ?>;
+	var src_arr = [];
+	function format_img_src(letter){
+		var this_letter = letter.toUpperCase();
+		if(this_letter == '&')
+			this_letter = 'ampersand';
+		else if(this_letter == '.')
+			this_letter = 'period';
+		else if(this_letter == ',')
+			this_letter = 'comma';
+		else if(this_letter == '#')
+			this_letter = 'hash';
+		else if(this_letter == '/')
+			this_letter = 'slash';
+		else if(this_letter == ' ')
+			return 'whitespace';
+		var this_max = filenum_arr[this_letter];
+		var letter_order = Math.floor(Math.random() * Math.floor(this_max));
+		var output = 'media/letters/'+this_letter+'-'+letter_order+'.jpg';
+		return output;
+	}
+	
+	// request json
+	if (window.XMLHttpRequest) { // Mozilla, Safari, IE7+ ...
+	    var httpRequest = new XMLHttpRequest();
+	} else if (window.ActiveXObject) { // IE 6 and older
+	    var httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	function loop_letters(c_letter, words, srcs, isInitialed = true){
+		// console.log(words[c_letter]);
+		if(!isInitialed)
+		{
+			var first_src = srcs[c_letter];
+			c_letter++;
+			if(c_letter >= words.length)
+				c_letter = 0;
+			var next_src = srcs[c_letter];
+			c_letter++;
+			if(c_letter >= words.length)
+				c_letter = 0;
+			var nextnext_src = srcs[c_letter];
+			if(first_src == 'whitespace')
+				radio_image_0.classList.add('whitespace');
+			else
+				radio_image_0.src = first_src;
+
+			if(next_src == 'whitespace')
+				radio_image_1.classList.add('whitespace');
+			else
+				radio_image_1.src = next_src;
+
+			if(nextnext_src == 'whitespace')
+				radio_image_2.classList.add('whitespace');
+			else
+				radio_image_2.src = nextnext_src;
+
+			setTimeout(function(){
+				radio_image_0.classList.remove('not_current');
+			}, 0);
+		}
+		else
+		{
+			image_counter++;
+			var current_image = radio_image[image_counter%3];
+			// var last_image = radio_image[(image_counter - 1) % 3];
+			var nextnext_image = radio_image[(image_counter + 2) % 3];
+			var nextnext_src = srcs[c_letter];
+
+			current_image.classList.remove('not_current');
+			// next_image.classList.add('not_current');
+			nextnext_image.classList.add('not_current');
+
+			if(nextnext_src == 'whitespace')
+				nextnext_image.classList.add('whitespace');
+			else{
+				nextnext_image.classList.remove('whitespace');
+				nextnext_image.src = nextnext_src;
+			}
+		}
+		
+		c_letter++;
+		if(c_letter >= words.length){
+			preload(0, radio_words, filenum_arr);
+			c_letter = 0;
+		}
+		return c_letter;
+	}
+	function preload(init_index = 0, words, num_arr){
+		src_arr = [];
+		var words_length = words.length;
+		var preload_index = init_index;
+		for(i = 0; i < words_length; i++)
+		{
+			var this_letter = words[i].toUpperCase();
+			// console.log(this_letter);
+			src_arr.push(format_img_src(this_letter));
+		}
+		var preload_img = new Image();
+		preload_img.onload = function(){
+			console.log('preload onload');
+			preload_index++;
+			if(preload_index < words_length)
+			{
+				preload_img.src = src_arr[preload_index];
+			}
+		}
+		// console.log(preload_index, src_arr[preload_index]);
+		preload_img.src = src_arr[preload_index];
+	}
+	httpRequest.onreadystatechange = function(){
+		
+		if (httpRequest.readyState === XMLHttpRequest.DONE) {
+			
+	      if (httpRequest.status === 200) {	
+      		var response = JSON.parse(httpRequest.responseText);
+      		
+      		if(response){
+      			current_letter = response['current_pos'];
+      			var wait = 1000 - (Date.now() % 1000);
+      			current_letter++;
+      			if(current_letter >= radio_words.length)
+      				current_letter = 0;
+      			preload(current_letter, radio_words);
+      			setTimeout(function(){
+      				current_letter = loop_letters(current_letter, radio_words, src_arr, false);
+      				// already current++ when initiating loop_letters();
+      				// so "current_letter" is actually the next next index to preload
+      				current_letter = (current_letter + 2) % radio_words.length;
+      				
+      				setInterval(function(){
+      					current_letter = loop_letters(current_letter, radio_words, src_arr);
+      				}, 1000);
+      				
+      			}, wait);
+      		}
+	      }
+	    }
+	};
+	httpRequest.open('GET', 'http://stray.o-r-g.net/now');
+	httpRequest.send();
+	[].forEach.call(radio_image, function(el, i){
+		el.addEventListener('click', open_duo);
+	});
+</script>
