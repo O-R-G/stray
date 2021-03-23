2f1e6a794595235a23b638e6f96f845ee1cfcac4
Wei-Hao Wang <wwang03@risd.edu>
2021-03-23 11:03:53 +0800

    checkout

--- views/print.php
+++ views/print.php
@@ -13,31 +13,52 @@ if($uri[1] == 'preview')
 </head>
 <body class="<?= $isPreview ? 'preview' : ''; ?>">    
     <script>  
-        Bindery.makeBook({
-            content: {
-                selector: '#print-container',
-                url: '/book'
-            },
-            pageSetup: {           
-                size: { width: '205mm', height: '297mm' },
-                // size: { width: '8.5in', height: '11in' },
-                margin: { 
-                    top: '15mm', 
-                    inner: '37mm', 
-                    outer: '15mm', 
-                    bottom: '0mm' 
+        window.addEventListener('load', function(){
+            Bindery.makeBook({
+                content: {
+                    selector: '.print-container',
+                    url: '/book'
                 },
-            },  
-            view: 
-                Bindery.View.FLIPBOOK,
-            rules: [
-                Bindery.PageBreak({ 
-                    selector: '.page', 
-                    position: 'both' 
-                }),
-                Bindery.RunningHeader({
-                }),
-            ],
+                pageSetup: {           
+                    size: { width: '210mm', height: '297mm' },
+                    // size: { width: '8.5in', height: '11in' },
+                    margin: { 
+                        top: '15mm', 
+                        inner: '37mm', 
+                        outer: '15mm', 
+                        bottom: '15mm' 
+                    },
+                },  
+                view: 
+                    Bindery.View.FLIPBOOK,
+                rules: [
+                    Bindery.PageBreak({ 
+                        selector: '.page', 
+                        position: 'both' 
+                    }),
+                    Bindery.RunningHeader({
+                    }),
+                ],
+            });
         });
+        
+        // Bindery.makeBook({ 
+        //     content: '.print-content',
+        //     view: Bindery.View.PRINT,
+        //     pageSetup: {
+        //         size: { width: '210mm', height: '297mm' },
+        //         margin: { top: '17.5mm', inner: '30mm', outer: '10mm', bottom: '17.5mm' },
+        //     },
+        //     printSetup: {
+        //         marks: Bindery.Marks.CROP,
+        //         bleed: '12pt',
+        //     },
+        //     rules: [
+        //       Bindery.PageBreak(
+        //         { selector: '.hotfield-break', position: 'after', continue:'left' }
+        //     ),
+        //     ], 
+
+        // });
     </script>
 </body>
