364aa3a7246347efb6d8210b6f3799078066c21d
Wei-Hao Wang <wwang03@risd.edu>
2021-07-30 15:25:01 +0800

    p outside of blockquote

--- views/image.php
+++ views/image.php
@@ -9,7 +9,10 @@ $urls = explode('/', 'text');
 $ids = $oo->urls_to_ids($urls);
 $item = $oo->get($ids[0]);
 // $body = $item['body'];
-$body = file_get_contents('static/txt/text.txt');
+if($uri[1] == 'mobile')
+	$body = file_get_contents('static/txt/text-mobile.txt');
+else
+	$body = file_get_contents('static/txt/text.txt');
 $texts = [];
 $chapters = explode('+++', $body);    
 foreach($chapters as $chapter) {
